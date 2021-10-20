5477fdcd6c0b19a310a4e9aad2035d534b1933ae
nikmmehr <hovalkafi@gmail.com>
2021-10-20 15:34:07 +0330

    blog category controller

---
+++ app/Http/Controllers/BlogCategoryController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BlogCategoryController extends Controller
+{
+    public function index()
+    {
+        return 'index';
+    }
+
+    public function show( $id )
+    {
+        return 'show';
+    }
+
+    public function save( Request $request )
+    {
+        return 'save';
+    }
+
+    public function update( Request $request, $id )
+    {
+        return 'update';
+    }
+
+    public function delete( $id )
+    {
+        return 'delete';
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SignRequestController;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\BlogCategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,23 +21,34 @@ use App\Http\Controllers\BlogController;
 /*
  * Sign requests
  */
-Route::prefix( '/signs' )->group( function() {
+Route::prefix( 'signs' )->group( function() {
     Route::get( '/', [ SignRequestController::class, 'index' ] );
-    Route::get( '/{id}', [ SignRequestController::class, 'show' ] );
-    Route::post( '/{id}', [ SignRequestController::class, 'confirm' ] );
-    Route::put( '/{id}', [ SignRequestController::class, 'update' ] );
-    Route::delete( '/{id}/{userId}', [ SignRequestController::class, 'delete' ] );
+    Route::get( '{id}', [ SignRequestController::class, 'show' ] );
+    Route::post( '{id}', [ SignRequestController::class, 'confirm' ] );
+    Route::put( '{id}', [ SignRequestController::class, 'update' ] );
+    Route::delete( '{id}/{userId}', [ SignRequestController::class, 'delete' ] );
 } );
 
 /*
  * Blog
  */
-Route::prefix( '/blog' )->group( function() {
+Route::prefix( 'blog' )->group( function() {
     Route::get( '/', [ BlogController::class, 'index' ] );
-    Route::get( '/{id}', [ BlogController::class, 'show' ] );
-    Route::post( '/{id}', [ BlogController::class, 'save' ] );
-    Route::put( '/{id}', [ BlogController::class, 'update' ] );
-    Route::delete( '/{id}', [ BlogController::class, 'delete' ] );
+    Route::get( '{id}', [ BlogController::class, 'show' ] );
+    Route::post( '{id}', [ BlogController::class, 'save' ] );
+    Route::put( '{id}', [ BlogController::class, 'update' ] );
+    Route::delete( '{id}', [ BlogController::class, 'delete' ] );
+} );
+
+/*
+ * Blog category
+ */
+Route::prefix( 'blog-category' )->group( function() {
+    Route::get( '/', [ BlogCategoryController::class, 'index' ] );
+    Route::get( '{id}', [ BlogCategoryController::class, 'show' ] );
+    Route::post( '{id}', [ BlogCategoryController::class, 'save' ] );
+    Route::put( '{id}', [ BlogCategoryController::class, 'update' ] );
+    Route::delete( '{id}', [ BlogCategoryController::class, 'delete' ] );
 } );
 
 Route::middleware( 'auth:sanctum' )->get( '/user', function( Request $request ) {
