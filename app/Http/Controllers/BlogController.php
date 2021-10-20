662e9b94b54c2c88960f053bc42f4379bc9efbf2
nikmmehr <hovalkafi@gmail.com>
2021-10-20 10:27:25 +0330

    create blog controller

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
-    //
+    public function index()
+    {
+        return 'index';
+    }
+
+    public function show( $id )
+    {
+        return 'show';
+    }
+
+    public function save( Request $request )
+    {
+        return 'save';
+    }
+
+    public function update( Request $request, $id )
+    {
+        return 'update';
+    }
+
+    public function delete( $id )
+    {
+        return 'delete';
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SignRequestController;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use App\Http\Controllers\BlogController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,15 +16,31 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-//
-Route::get( '/signs', [ SignRequestController::class, 'index' ] );
-Route::get( '/signs/{id}', [ SignRequestController::class, 'show' ] );
-Route::put( '/signs/{id}', [ SignRequestController::class, 'update' ] );
-Route::delete( '/signs/{id}/{userId}', [ SignRequestController::class, 'delete' ] );
 
+/*
+ * Sign requests
+ */
+Route::prefix( '/signs' )->group( function() {
+    Route::get( '/', [ SignRequestController::class, 'index' ] );
+    Route::get( '/{id}', [ SignRequestController::class, 'show' ] );
+    Route::post( '/{id}', [ SignRequestController::class, 'confirm' ] );
+    Route::put( '/{id}', [ SignRequestController::class, 'update' ] );
+    Route::delete( '/{id}/{userId}', [ SignRequestController::class, 'delete' ] );
+} );
+
+/*
+ * Blog
+ */
+Route::prefix( '/blog' )->group( function() {
+    Route::get( '/', [ BlogController::class, 'index' ] );
+    Route::get( '/{id}', [ BlogController::class, 'show' ] );
+    Route::post( '/{id}', [ BlogController::class, 'save' ] );
+    Route::put( '/{id}', [ BlogController::class, 'update' ] );
+    Route::delete( '/{id}', [ BlogController::class, 'delete' ] );
+} );
 
 Route::middleware( 'auth:sanctum' )->get( '/user', function( Request $request ) {
     return $request->user();
 } );
 
-Route::any( '/{a?}/{b?}/{c?}', function() { throw new NotFoundHttpException(''); } );
+Route::any( '/{a?}/{b?}/{c?}', function() { throw new NotFoundHttpException( '' ); } );
